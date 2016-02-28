a7e22fdfa016c4d11f883a87ea7ab7b8e9d6ba0e
zhangzewei <zewei.zhang@stonybrook.edu>
2016-02-28 15:13:48 -0500

    perfect page structure

---
+++ web/bootstrap-3.3.6-dist/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.header').load("header.html");
+  $('.navigation').load("nav.html");
+  $('.banner').load("banner.html");
+  $('.searchbar').load("searchbar.html");
+  $('.gallery').load("gallery.html");
+  $('.footer').load("footer.html");
+});
+
